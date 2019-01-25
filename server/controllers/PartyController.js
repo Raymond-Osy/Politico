323a3d571e683b4fb2e6baaa7446a25d2784cf0c
Raymond Osy <raymondosy1@gmail.com>
2019-01-25 05:26:51 +0100

    feature(getPartiesById) API endpiont for getting a single party - Add parties route to handle an incoming request to this endpoint - Add parties controller that contains logic for the response

--- server/controllers/PartyController.js
+++ server/controllers/PartyController.js
@@ -13,6 +13,21 @@ class PartyController {
       data: parties
     });
   }
+
+  static getAPartyById(req, res) {
+    const { id } = req.params;
+    const party = parties.find(p => p.id === parseInt(req.params.id));
+    if (!party) {
+      return res.status(404).json({
+        status: 404,
+        error: `Party with the given Id ${id} does not exist`
+      });
+    }
+    return res.status(200).json({
+      status: 200,
+      data: party,
+    });
+  }
 }
 
 export default PartyController;
