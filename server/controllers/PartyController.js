258f01dd3ce0a25f077300b91286613aa121eff0
Raymond Osy <raymondosy1@gmail.com>
2019-01-25 06:35:06 +0100

    feature(createParty): API endpoint for creating party - Add parties route to handle post request to the endpoint - Add parties controller that contains logic for the response [Finishes #163310608]

--- server/controllers/PartyController.js
+++ server/controllers/PartyController.js
@@ -13,6 +13,21 @@ class PartyController {
       data: parties
     });
   }
+
+  static createParty(req, res) {
+    const party = {
+      id: parties.length === 0 ? 1 : parties.length + 1,
+      name: req.body.name,
+      hqAddress: req.body.hqAddress,
+      logoUrl: req.body.logoUrl
+    };
+
+    parties.push(party);
+    return res.status(201).send({
+      status: 201,
+      data: parties,
+    });
+  }
 }
 
 export default PartyController;
--- server/routes/parties.js
+++ server/routes/parties.js
@@ -2,11 +2,12 @@ import express from 'express';
 import PartyController from '../controllers/PartyController';
 
 const {
-  getAllParties
+  getAllParties, createParty
 } = PartyController;
 
 const partyRouter = express.Router();
 
 partyRouter.get('/', getAllParties);
+partyRouter.post('/', createParty);
 
 export default partyRouter;
