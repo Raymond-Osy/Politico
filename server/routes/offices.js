8d8f3992d30e1a5c13b8f8683ec09684c2805143
Raymond Osy <raymondosy1@gmail.com>
2019-01-26 10:01:20 +0100

    API endpoint for getting a single office - endpoint for getting a single office [Delivers #163497840]

--- server/routes/offices.js
+++ server/routes/offices.js
@@ -2,11 +2,12 @@ import express from 'express';
 import OfficeController from '../controllers/OfficeController';
 
 const {
-  getAllOffices
+  getAllOffices, getAnOfficeById
 } = OfficeController;
 
 const officeRouter = express.Router();
 
 officeRouter.get('/', getAllOffices);
+officeRouter.get('/:id', getAnOfficeById);
 
 export default officeRouter;
