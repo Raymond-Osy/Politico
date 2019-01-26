b7ad423f31bc44969dd31ddb472d979c690f0151
Raymond Osy <raymondosy1@gmail.com>
2019-01-26 10:23:07 +0100

    feature(createOffice endpoint): API endpoint for creating an office - endpoint that creates an office [Delivers #63498012]

--- server/routes/offices.js
+++ server/routes/offices.js
@@ -2,12 +2,13 @@ import express from 'express';
 import OfficeController from '../controllers/OfficeController';
 
 const {
-  getAllOffices, getAnOfficeById
+  getAllOffices, getAnOfficeById, createOffices
 } = OfficeController;
 
 const officeRouter = express.Router();
 
 officeRouter.get('/', getAllOffices);
 officeRouter.get('/:id', getAnOfficeById);
+officeRouter.post('/', createOffices);
 
 export default officeRouter;
